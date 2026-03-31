e02474313e6f5bfed4ecd350c36ad56e6727446d
Kshrutika Singh <kshrutika.24bce10496@vitbhopal.ac.in>
2026-03-31 18:57:49 +0530

    Added login method in UserDAO

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,43 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         UserDAO userDAO = new UserDAO();
 
-        System.out.println("=== USER REGISTRATION ===");
+        System.out.println("=== HELP DESK SYSTEM ===");
+        System.out.println("1. Register");
+        System.out.println("2. Login");
 
-        System.out.print("Enter Name: ");
-        String name = sc.nextLine();
+        System.out.print("Enter choice: ");
+        int choice = sc.nextInt();
+        sc.nextLine();
 
-        System.out.print("Enter Email: ");
-        String email = sc.nextLine();
+        if (choice == 1) {
 
-        System.out.print("Enter Password: ");
-        String password = sc.nextLine();
+            System.out.print("Enter Name: ");
+            String name = sc.nextLine();
 
-        User user = new User(name, email, password);
+            System.out.print("Enter Email: ");
+            String email = sc.nextLine();
 
-        userDAO.register(user);
+            System.out.print("Enter Password: ");
+            String password = sc.nextLine();
+
+            User user = new User(name, email, password);
+            userDAO.register(user);
+
+        } else if (choice == 2) {
+
+            System.out.print("Enter Email: ");
+            String email = sc.nextLine();
+
+            System.out.print("Enter Password: ");
+            String password = sc.nextLine();
+
+            boolean success = userDAO.login(email, password);
+
+            if (success) {
+                System.out.println("Login Successful ✅");
+            } else {
+                System.out.println("Invalid Credentials ❌");
+            }
+        }
     }
 }
--- src/UserDAO.java
+++ src/UserDAO.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class UserDAO {
 
@@ -20,6 +21,26 @@ public class UserDAO {
 
         } catch (Exception e) {
             System.out.println(e);
+        }
+    }
+
+    public boolean login(String email, String password) {
+        try {
+            Connection con = DBConnection.getConnection();
+
+            String query = "SELECT * FROM users WHERE email=? AND password=?";
+            PreparedStatement ps = con.prepareStatement(query);
+
+            ps.setString(1, email);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            return rs.next(); // true if user found
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
         }
     }
 }
